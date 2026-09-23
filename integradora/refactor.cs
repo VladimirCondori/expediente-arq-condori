07b197335fecdc47aa7d04fd260d4f7763751d5e
Vladimir <vladylu05@gmail.com>
2026-09-22 20:13:10 -0400

    Parte 2: refactor DIP

---
+++ integradora/refactor.cs
@@ -0,0 +1,68 @@
+// Refactor: Vladimir Condori
+
+using System;
+
+namespace Integradora.Comedor;
+
+public interface IRepositorioPedidos
+{
+    void GuardarPedido(string estudiante, string menu, int cantidad, decimal total);
+}
+
+public interface INotificador
+{
+    void Enviar(string mensaje);
+}
+
+public class BaseDeDatosComedor : IRepositorioPedidos
+{
+    public void GuardarPedido(string estudiante, string menu, int cantidad, decimal total)
+        => Console.WriteLine($"[BD] INSERT INTO pedidos VALUES ('{estudiante}', '{menu}', {cantidad}, {total})");
+}
+
+public class CorreoUniversitario : INotificador
+{
+    public void Enviar(string mensaje) => Console.WriteLine($"[CORREO] {mensaje}");
+}
+
+public class GestorDePedidos
+{
+    private readonly IRepositorioPedidos _repositorio;
+    private readonly INotificador _notificador;
+
+    public GestorDePedidos(IRepositorioPedidos repositorio, INotificador notificador)
+    {
+        _repositorio = repositorio;
+        _notificador = notificador;
+    }
+
+    public void ProcesarPedido(string estudiante, string tipoMenu, int cantidad)
+    {
+        decimal precioBase;
+        switch (tipoMenu)
+        {
+            case "estandar":    precioBase = 12; break;
+            case "vegetariano": precioBase = 14; break;
+            case "beca":        precioBase = 5;  break;
+            default:            precioBase = 12; break;
+        }
+        decimal total = precioBase * cantidad;
+
+        _repositorio.GuardarPedido(estudiante, tipoMenu, cantidad, total);
+
+        Console.WriteLine("----- VALE DE COMEDOR -----");
+        Console.WriteLine($"{estudiante}: {cantidad} x menú {tipoMenu}");
+        Console.WriteLine($"TOTAL: {total:0.00} Bs");
+
+        _notificador.Enviar($"Pedido registrado: {cantidad} x {tipoMenu}, {estudiante}");
+    }
+}
+
+public static class Demo
+{
+    public static void Correr()
+    {
+        var gestor = new GestorDePedidos(new BaseDeDatosComedor(), new CorreoUniversitario());
+        gestor.ProcesarPedido("Noelia", "vegetariano", 2);
+    }
+}
